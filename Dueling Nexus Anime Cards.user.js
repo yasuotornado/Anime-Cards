6a84f03b4af3bbbaec21c5f1d63b60b981b1ba73
yasuotornado <58336785+yasuotornado@users.noreply.github.com>
2019-12-28 09:53:48 -0600

    Add files via upload

---
+++ Dueling Nexus Anime Cards.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Dueling Nexus Anime Cards
+// @include      https://duelingnexus.com/*
+// @namespace    https://duelingnexus.com/
+// @version      0.2
+// @description  Anime Cards for DuelingNexus
+// @resource      CARDS1 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%201
+// @resource      CARDS2 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%202
+// @resource      CARDS3 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%203
+// @resource      CARDS4 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%204
+// @resource      CARDS5 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%205
+// @resource      CARDS6 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%206
+// @resource      CARDS7 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%207
+// @resource      CARDS8 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%208
+// @resource      CARDS9 https://github.com/yasuotornado/Anime-Cards/raw/master/scripts/Ultimate%20Anime%20Card%20Pack%209
+// @grant         GM_addStyle
+// @grant         GM_getResourceText
+//
+// ==/UserScript==
+GM_addStyle(GM_getResourceText('CARDS1'));
+GM_addStyle(GM_getResourceText('CARDS2'));
+GM_addStyle(GM_getResourceText('CARDS3'));
+GM_addStyle(GM_getResourceText('CARDS4'));
+GM_addStyle(GM_getResourceText('CARDS5'));
+GM_addStyle(GM_getResourceText('CARDS6'));
+GM_addStyle(GM_getResourceText('CARDS7'));
+GM_addStyle(GM_getResourceText('CARDS8'));
+GM_addStyle(GM_getResourceText('CARDS9'));
